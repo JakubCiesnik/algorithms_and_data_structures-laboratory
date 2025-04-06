43595ea45b0b50970d10d370a1615a1b9dd85286
Vulpine <jakub.ciesnik@protonmail.com>
2025-04-06 17:11:19 +0000

    setup

---
+++ lab/sorting/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iterator>
+
+int main () {
+  int x = 0;
+
+  if (x == 5)
+  {
+    std::cout << "x is 5" << std::endl;
+  } 
+  else 
+  {
+    std::cout << "x is not 5" << std::endl;
+  }
+  return 0;
+}
