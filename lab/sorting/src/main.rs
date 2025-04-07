f32074d3689ef38cf6d396693c03b8d051c2df90
Vulpine <jakub.ciesnik@protonmail.com>
2025-04-07 11:27:35 +0000

    figured out random array generation

--- lab/sorting/src/main.rs
+++ lab/sorting/src/main.rs
@@ -1,14 +1,29 @@
 use rand::prelude::*;
+use rand::rng;
 use std::fs::File;
 use std::io;
-use std::io::prelude::*;
+// use std::io::prelude::*;
+
+// fn writetofile() -> io::Result<()> {
+//     Ok(())
+// }
+//
+// fn parseinputdata() -> io::Result<()> {
+//     Ok(())
+// }
+//
+// fn generatedata() {}
 
 fn main() -> io::Result<()> {
-    let mut f = File::create("datasets/test/foo.txt")?;
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    f.write_all(b"{secret_number}")?; // needs to be b"" to convert the string slice (&str) to byte
-                                      // slice, which isn't required to be valid UTF8. this string
-                                      // type is used in OS operations
+    // let mut f = File::create("datasets/test/foo.txt")?;
+    const N: usize = 3200;
+    let arr1: [u32; N] = rng().random();
+    println!("{:?}", &arr1[..N]);
+
+    // f.write_all(b"{secret_number}")?;
+    // needs to be b"" to convert the string slice (&str) to byte
+    // slice, which isn't required to be valid UTF8. this string
+    // type is used in OS operations
     Ok(())
 }
 
